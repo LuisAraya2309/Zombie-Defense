433fadd5de6eb93d9eb4c95b65678009ce1181a0
Luis <luistec2309@estudiantec.cr>
2020-10-21 15:04:21 -0600

    Descripcion de preview de heroes

--- ZombieDefense/src/zombiedefense/PreviewHeroes.java
+++ ZombieDefense/src/zombiedefense/PreviewHeroes.java
@@ -53,45 +53,45 @@ public class PreviewHeroes extends javax.swing.JFrame {
         getContentPane().add(jLabelRegresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, -10, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ba26dbae7487041f4d7c1948cbb3bccb.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, 170, 240));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 10, 170, 240));
 
         jTextArea1.setBackground(new java.awt.Color(51, 51, 51));
         jTextArea1.setColumns(20);
         jTextArea1.setFont(new java.awt.Font("Felix Titling", 1, 14)); // NOI18N
         jTextArea1.setForeground(new java.awt.Color(255, 204, 102));
         jTextArea1.setRows(5);
-        jTextArea1.setText("The Revenant es un \nheroe blindado");
+        jTextArea1.setText("The Revenant es un \nheroe blindado que \ntiene una cantidad \nde escudo adicional \na la vidaactual.\n");
         jScrollPane1.setViewportView(jTextArea1);
         jTextArea1.setEditable(false);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 410, 170, 170));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, 170, 170));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/capitan-america-el-soldado-de-invierno-bucky-barnes-papel-pintado-15598_L.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 130, 170, 240));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 10, 170, 240));
 
         jTextArea2.setBackground(new java.awt.Color(51, 51, 51));
         jTextArea2.setColumns(20);
         jTextArea2.setFont(new java.awt.Font("Felix Titling", 1, 14)); // NOI18N
         jTextArea2.setForeground(new java.awt.Color(255, 204, 102));
         jTextArea2.setRows(5);
-        jTextArea2.setText("Scout es un sigiloso\nexplorador");
+        jTextArea2.setText("Scout es un sigiloso\nexplorador que \nposee un arma a \nlargo alcance y un \nrango de vision\nmayor a los demas.");
         jScrollPane2.setViewportView(jTextArea2);
         jTextArea2.setEditable(false);
 
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 410, 170, 170));
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 170, 170));
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/f23be9a99232c9e58ec9a393e64fb1fc.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 130, 170, 240));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 10, 170, 240));
 
         jTextArea3.setBackground(new java.awt.Color(51, 51, 51));
         jTextArea3.setColumns(20);
         jTextArea3.setFont(new java.awt.Font("Felix Titling", 1, 14)); // NOI18N
         jTextArea3.setForeground(new java.awt.Color(255, 204, 102));
         jTextArea3.setRows(5);
-        jTextArea3.setText("Momon");
+        jTextArea3.setText("Dheylo es un asesino \ngenuino que tiene \nun daño muy potente\nque acaba de manera\nfácil con los zombies.\n");
         jScrollPane3.setViewportView(jTextArea3);
 
-        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 410, 170, 170));
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 260, 170, 170));
 
         jLabelFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/apocalipsis_nostradamus_biblia_fin_del_mundo_mayas.jpg_759710130.jpg"))); // NOI18N
         getContentPane().add(jLabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1050, 600));
